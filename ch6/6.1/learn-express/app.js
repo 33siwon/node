05a1ce724752a81e03666bf79c28598fa8b772b0
siwon <siwon3376@gmail.com>
2021-03-27 09:40:14 +0900

    'middleware'

--- ch6/6.1/learn-express/app.js
+++ ch6/6.1/learn-express/app.js
@@ -1,14 +1,39 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require("path")
+const { nextTick } = require('process')
+const app = express()
 
-const app = express();
-app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-  // res.send('Hello, Express');
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+app.set('port', process.env.PORT || 3000)
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
-});
+app.use((req,res,next)=>{
+  console.log('hi')
+  next()
+})
+
+app.get('*', (req,res)=>{
+  console.log('hello every ')
+})
+
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname,'index.html'))
+})
+
+app.post('/', (req,res)=>{
+  res.send('hello express')
+})
+
+app.get('/about', (req,res)=>{
+  res.sendFile(path.join(__dirname,'index.html'))
+})
+
+
+app.get('/category/:name',(req,res)=>{
+  console.log(`hello wildcard`)
+})
+
+
+
+app.listen(app.get('port'),()=>{
+  console.log('익스프레스 서버 실행 실행')
+})
