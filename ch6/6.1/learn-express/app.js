8d70cd244077df8b52022e5fb6845deffd63651f
siwon <siwon3376@gmail.com>
2021-03-28 16:34:01 +0900

    'middleware'

--- ch6/6.1/learn-express/app.js
+++ ch6/6.1/learn-express/app.js
@@ -1,44 +1,38 @@
 const express = require('express')
-const path = require("path")
-const { nextTick } = require('process')
-const app = express()
+const path = require('path')
 
+const app = express()
 
 app.set('port', process.env.PORT || 3000)
 
 app.use((req,res,next)=>{
-  console.log('hi')
+  console.log('모든 요청에서 다 실행됩니다.')
   next()
 })
 
-app.use((req,res,next)=>{
-  res.sendFile(path.join(__dirname,'index1.html'))
+app.get('/', (req,res,next)=>{
+  console.log('GET / 요청에서만 실행됩니다.')
+  // res.setHeader('Content-Type','text/html')
+  res.send('홈 화면 입니다.')
   next()
+}
+)
+app.get('/about',(req,res)=>{
+      res.sendFile(path.join(__dirname,'/index.html'))
+})
+app.post('/',(req,res)=>{
+  res.send('hello world')
 })
 
-app.get('*', (req,res)=>{
-  res.sendFile(path.join(__dirname,'index1.html'))
+// app.use((req,res,next)=>{
+//   res.send('404입니다만')
+// })
+
+app.use((err,req,res,next)=>{
+  console.log(err)
+  res.send('hey error')
 })
 
-app.get('/', (req,res)=>{
-  res.sendFile(path.join(__dirname,'index.html'))
-})
-
-app.post('/', (req,res)=>{
-  res.send('hello express')
-})
-
-app.get('/about', (req,res)=>{
-  res.send('hi')
-})
-
-
-app.get('/category/:name',(req,res)=>{
-  console.log(`hello wildcard`)
-})
-
-
-
-app.listen(app.get('port'),()=>{
-  console.log('익스프레스 서버 실행 실행')
+app.listen(app.get('port'), (req,res)=>{
+  console.log('익스프레스 서버 실행')
 })
